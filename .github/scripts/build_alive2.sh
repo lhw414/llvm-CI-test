fa00f119bc7a7878a6448332f8f8c7680d560ded
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-21 00:33:10 +0900

    Fix path2

--- .github/scripts/build_alive2.sh
+++ .github/scripts/build_alive2.sh
@@ -7,10 +7,7 @@ cd alive2
 git checkout 13266e9e163c2984f3c5de0c0961e04cb4cafc7a
 cmake -GNinja -Bbuild \
     -DBUILD_TV=ON \
-    -DCMAKE_C_COMPILER=$CLANG \
-    -DCMAKE_CXX_COMPILER=$CLANGXX \
-    -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
-    -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++ -fuse-ld=$LLD" \
-    -DCMAKE_PREFIX_PATH="$LLVM_DIR;$Z3_DIR" \
+    -DCMAKE_PREFIX_PATH="/opt/llvm-18.1.0;/opt/z3-4.13.0" \
+    -DZ3_INCLUDE_DIR=/opt/z3-4.13.0/include \
     -DCMAKE_BUILD_TYPE=Release
 cmake --build build
