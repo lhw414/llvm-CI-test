e4bb9a1ada982cd096b43cec2eb61cf2f983831d
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-24 11:58:49 +0900

    add github workspace

--- .github/scripts/test_benchmark.sh
+++ .github/scripts/test_benchmark.sh
@@ -6,8 +6,14 @@ cd benchmarks
 
 echo "Start testing using benchmarks..."
 
+if [ -f "/compiler/build/swpp-compiler" ]; then
+    echo "swpp-compiler found."
+else
+    echo "swpp-compiler not found."
+fi
+
 # Build asms and capture output
-output=$(python3 build-asms.py /compiler/build/swpp-compiler 2>&1)
+output=$(python3 build-asms.py ${GITHUB_WORKSPACE}/compiler/build/swpp-compiler 2>&1)
 exit_code=$?
 
 if [ $exit_code -ne 0 ] || [[ $output == *"Failed to build ll from"* ]]; then
