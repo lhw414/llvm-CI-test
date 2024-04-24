45cf863eed0455f0ea8fe8dcb03bd5018fc3f798
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-24 11:55:14 +0900

    Edit path

--- .github/scripts/test_benchmark.sh
+++ .github/scripts/test_benchmark.sh
@@ -2,21 +2,12 @@
 
 LLVM_DIR=/opt/llvm-18.1.0/bin/
 
-cd benchmarks/
+cd benchmarks
 
 echo "Start testing using benchmarks..."
 
-# Check for Python3 installation and install if missing
-if ! command -v python3 &>/dev/null; then
-    echo "Python3 is not installed. Installing Python3..."
-    sudo apt-get update && sudo apt-get install -y python3
-else
-    echo "Python3 is installed. Version info:"
-    python3 --version
-fi
-
 # Build asms and capture output
-output=$(python3 build-asms.py compiler/build/swpp-compiler 2>&1)
+output=$(python3 build-asms.py /compiler/build/swpp-compiler 2>&1)
 exit_code=$?
 
 if [ $exit_code -ne 0 ] || [[ $output == *"Failed to build ll from"* ]]; then
