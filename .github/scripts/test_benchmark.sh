d03bdcd211ec482afbbb5b6eaae926e2deb2f839
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-24 11:39:07 +0900

    Edit scripts

--- .github/scripts/test_benchmark.sh
+++ .github/scripts/test_benchmark.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 LLVM_DIR=/opt/llvm-18.1.0/bin/
-ALIVE_TV_BINARY=$(realpath "$1"/alive-tv)
 
 cd benchmarks/
 
@@ -9,9 +8,11 @@ echo "Start testing using benchmarks..."
 
 # Build asms and capture output
 output=$(python3 build-asms.py compiler/build/swpp-compiler 2>&1)
-# exit_code=$?
+exit_code=$?
 
-# if [ $exit_code -ne 0 ] || [[ $output == *"Failed to build ll from"* ]]; then
-#     echo "Failed to build ll using swpp-compiler."
-#     exit 1
-# fi
+if [ $exit_code -ne 0 ] || [[ $output == *"Failed to build ll from"* ]]; then
+    echo "Failed to build ll using swpp-compiler: $output"
+    exit 1
+fi
+
+echo "Benchmarks built successfully."
