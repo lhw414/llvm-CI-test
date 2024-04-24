9c6f1f2666315483149b455104c83e96ece3bc27
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-24 11:25:09 +0900

    Edit test benchmark scripts

--- .github/scripts/test_benchmark.sh
+++ .github/scripts/test_benchmark.sh
@@ -5,11 +5,8 @@ ALIVE_TV_BINARY=$(realpath "$1"/alive-tv)
 
 cd benchmarks/
 
-# Build lls
-python3 build-lls.py "${LLVM_DIR}"
-
 # Build asms and capture output
-output=$(python3 build-asms.py ../build/swpp-compiler 2>&1)
+output=$(python3 build-asms.py compiler/build/swpp-compiler 2>&1)
 exit_code=$?
 
 if [ $exit_code -ne 0 ] || [[ $output == *"Failed to build ll from"* ]]; then
