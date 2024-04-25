fae62cc4249838f9a1aecf1c2022835cb0315a2c
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-25 16:22:33 +0900

    fix test

--- do_tests.sh
+++ do_tests.sh
@@ -3,15 +3,18 @@
 # unit_tests 디렉토리 내 모든 서브디렉토리 순회
 for dir in unit_tests/*/;
 do
-    if [ -f "${dir}input1.ll" ]; then
+    if [ -f "${dir}input.ll" ]; then
         echo "Testing in $dir"
 
-        opt -S -passes="test-optimization" "${dir}input.ll" -o "${dir}output.ll"
+        # 최적화 명령 실행 및 오류 로그 저장
+        opt -S -passes="test-optimization" "${dir}input.ll" -o "${dir}output.ll" 2>"${dir}error.log"
 
-        if [ -f "${dir}output.ll" ]; then
+        # opt 명령의 종료 코드 확인
+        if [ $? -eq 0 ] && [ -s "${dir}output.ll" ]; then
             echo "Test PASSED for ${dir}input.ll"
         else
             echo "Test FAILED for ${dir}input.ll"
+            cat "${dir}error.log"
         fi
     fi
 done
