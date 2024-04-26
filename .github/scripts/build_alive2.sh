f89a7c015792b27806783bd564f669981d3a3540
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-27 03:13:45 +0900

    fix scripts

--- .github/scripts/build_alive2.sh
+++ .github/scripts/build_alive2.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-echo "Current ls:"
-ls
-
-# /opt 디렉토리의 내용 확인
-echo "Contents of /opt:"
-ls /opt/
-
 # LLVM의 clang 경로를 PATH에 추가
 export PATH=/opt/llvm-18.1.0/bin:$PATH
+
+# clang 및 clang++ 버전 확인
+echo "clang version:"
+clang --version
+echo "clang++ version:"
+clang++ --version
 
 git clone https://github.com/AliveToolkit/alive2.git
 cd alive2
 
-# 현재 디렉토리 출력
-echo "Current directory:"
-pwd
-
-# Build Alive2
 git checkout 13266e9e163c2984f3c5de0c0961e04cb4cafc7a
 git apply ../compiler/.github/scripts/alive2-swpp2024-intrinsics.patch
 
-# cmake를 사용하여 빌드 설정, LLVM의 clang과 clang++를 사용
+# CMake 설정과 빌드
 cmake -GNinja -Bbuild \
     -DBUILD_TV=ON \
     -DCMAKE_PREFIX_PATH="/opt/llvm-18.1.0;/opt/z3-4.13.0" \
