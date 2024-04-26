0435bfd06ae9a70a1c8e28094a3af4f39bf3a863
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-26 11:42:39 +0900

    d

--- .github/scripts/build_alive2.sh
+++ .github/scripts/build_alive2.sh
@@ -9,7 +9,7 @@ pwd
 
 # Build Alive2
 git checkout 13266e9e163c2984f3c5de0c0961e04cb4cafc7a
-git apply /compiler/.github/scripts/alive2-swpp2024-intrinsics.patch
+git apply ../.github/scripts/alive2-swpp2024-intrinsics.patch
 cmake -GNinja -Bbuild \
     -DBUILD_TV=ON \
     -DCMAKE_PREFIX_PATH="/opt/llvm-18.1.0;/opt/z3-4.13.0" \
