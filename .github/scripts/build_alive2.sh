ea9d06b5b0dded18ad02499cb75eda6d66e76f1b
lhw414 <dlgusdn0414@snu.ac.kr>
2024-04-27 16:49:52 +0900

    fix alive build scripts

--- .github/scripts/build_alive2.sh
+++ .github/scripts/build_alive2.sh
@@ -6,7 +6,7 @@ export PATH=/opt/llvm-18.1.0/bin:$PATH
 git clone https://github.com/AliveToolkit/alive2.git
 cd alive2
 
-# git checkout 13266e9e163c2984f3c5de0c0961e04cb4cafc7a
+git checkout 13266e9e163c2984f3c5de0c0961e04cb4cafc7a
 git apply ../compiler/.github/scripts/alive2-swpp2024-intrinsics.patch
 
 # CMake 설정과 빌드
@@ -15,6 +15,8 @@ cmake -GNinja -Bbuild \
     -DCMAKE_PREFIX_PATH="/opt/llvm-18.1.0;/opt/z3-4.13.0" \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+    -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++ -fuse-ld=/opt/llvm-18.1.0/bin/ld.lld" \
     -DZ3_INCLUDE_DIR=/opt/z3-4.13.0/include \
     -DZ3_LIBRARIES=/opt/z3-4.13.0/lib/libz3.so \
     -DCMAKE_BUILD_TYPE=Release
